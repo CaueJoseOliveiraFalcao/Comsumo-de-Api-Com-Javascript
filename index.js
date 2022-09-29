a392cb54ec8d6e525c188bd5e3a36e22008f9f60
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-09-29 12:36:03 -0300

    ne

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict"
-const cors = "cors"
+
 
 const pesquisarCep = async() =>{
     const cep =  document.getElementById("cep").value;
-    const url = ` viacep.com.br/ws/01001000/json/`
+    const url = ` viacep.com.br/ws/${cep}/json/`
     const dados = await fetch(url)
     const endereco = await dados.json();
     console.log (endereco.cep);
 }
-document.getElementById("cep").addEventListener('focusout' , pesquisarCep());
+document.getElementById("cep").addEventListener('focusout' , pesquisarCep);
